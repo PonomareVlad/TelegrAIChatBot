9264549619f06c8456b1e2503d9c5a25cb25587a
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-13 18:40:32 +0500

    Messages history

--- src/bot.mjs
+++ src/bot.mjs
@@ -5,18 +5,46 @@ import {freeStorage} from "@grammyjs/storage-free";
 export const ai = new OpenAI(process.env.OPENAI_API_KEY);
 export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.use(session({storage: freeStorage(bot.token)}));
+bot.use(session({
+    initial: () => ({messages: []}),
+    storage: freeStorage(bot.token)
+}));
+
+bot.command("start", ctx => {
+    const message = ctx?.session?.messages?.length ?
+        "You started a new conversation, the story was deleted." :
+        "Send any text message to start conversation."
+    ctx.session.messages = [];
+    return ctx.reply(message);
+});
 
 bot.on("message:text", async ctx => {
-    const {msg: {text: prompt}} = ctx;
-    console.log("User:", prompt);
+    const {
+        msg: {
+            text: content = ""
+        },
+        session: {
+            messages = []
+        }
+    } = ctx;
+    console.log("User:", content);
+    messages.push({role: "user", content});
     await ctx.replyWithChatAction("typing");
     try {
-        const {choices: [{message: {content}}]} = await ai.chat({prompt});
+        const {
+            choices: [
+                {
+                    message: {
+                        content = ""
+                    } = {}
+                } = {}
+            ] = []
+        } = await ai.chat({messages});
+        messages.push({role: "assistant", content});
         console.log("Assistant:", content);
-        await ctx.reply(content);
+        return ctx.reply(content);
     } catch (e) {
-        await ctx.reply(e.message || e);
+        return ctx.reply(e.message || e);
     }
 });
 
--- src/openai.mjs
+++ src/openai.mjs
@@ -1,3 +1,15 @@
+export const sanitizeMessages = messages => {
+    const map = ({role, content} = {}) => ({role, content});
+    const filter = ({role, content} = {}) => role && content;
+    return messages.map(map).filter(filter);
+}
+
+export const setSystem = (content = "", messages = []) => {
+    let system = messages.find(({role} = {}) => role === "system");
+    if (!system) messages.unshift(system = {});
+    return Object.assign(system, {role: "system", content});
+}
+
 export default class OpenAI {
     options = {
         api: "https://api.openai.com/v1/"
@@ -8,29 +20,23 @@ export default class OpenAI {
         else Object.assign(this.options, options);
     }
 
-    static sanitizeMessages = messages => {
-        const map = ({role, content} = {}) => ({role, content});
-        const filter = ({role, content} = {}) => role && content;
-        return messages.map(map).filter(filter);
-    }
-
     async chat(options = {}) {
         const {
             api = "",
             token = "",
             system = "",
             prompt = "",
-            max_tokens = 4000,
+            max_tokens = 1000,
             model = "gpt-3.5-turbo",
             path = "chat/completions",
             messages = [
-                {role: "system", content: system},
                 {role: "user", content: prompt}
             ]
         } = {
             ...this.options,
             ...options
         };
+        if (system) setSystem(system, messages);
         const url = new URL(path, api);
         const headers = {
             "Content-Type": "application/json",
@@ -39,7 +45,7 @@ export default class OpenAI {
         const body = JSON.stringify({
             model,
             max_tokens,
-            messages: this.constructor.sanitizeMessages(messages),
+            messages: sanitizeMessages(messages),
         });
         const response = await fetch(url, {method: "post", headers, body});
         const data = await response.json();
