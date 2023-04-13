1366687a36a6d0f0cf793cf97abfafb2a5b02be5
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-13 16:52:18 +0500

    Sessions test

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,16 @@
-import {Bot} from "grammy/web";
+import {Bot, session} from "grammy/web";
+import {freeStorage} from "@grammyjs/storage-free";
 
 export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("message:text", async ctx => ctx.reply(ctx.msg.text));
+bot.use(session({
+    initial: () => ({count: 0}),
+    storage: freeStorage(bot.token),
+}));
+
+bot.on("message:text", async ctx => {
+    ctx.session.count++;
+    await ctx.reply(`Message count: ${ctx.session.count}`);
+});
 
 export default bot;
