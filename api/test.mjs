981ce653dd8231b09590d80561e2021685499f3a
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 13:34:03 +0500

    Fix

--- api/test.mjs
+++ api/test.mjs
@@ -7,14 +7,13 @@ const setTimeoutAsync = promisify((a, f) => setTimeout(f, a));
 export default async ({url}) => {
     const start = Date.now();
     const encoder = new TextEncoder();
-    const delay = new URL(url).searchParams.get("delay") || 0;
+    let seconds = parseInt(new URL(url).searchParams.get("seconds") || 1);
     const stream = new ReadableStream({
-        start: controller => controller.enqueue(encoder.encode(delay + ":")),
+        start: controller => controller.enqueue(encoder.encode(String(seconds))),
         async pull(controller) {
-            await setTimeoutAsync(delay);
-            controller.enqueue(encoder.encode(String(Date.now() - start)));
+            if (seconds-- < 1) return controller.close();
             await setTimeoutAsync(1000);
-            return controller.close();
+            return controller.enqueue(encoder.encode(":" + seconds));
         }
     });
     return new Response(stream);
