c2a5b247ce0a4b5f17cb901941913a973e63bc90
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 14:21:27 +0500

    Test hybrid version

--- api/hybrid.mjs
+++ api/hybrid.mjs
@@ -9,6 +9,14 @@ export default async ({url}, ctx) => {
     let time = 0;
     let limit = 55;
     let seconds = parseInt(new URL(url).searchParams.get("seconds") || 100);
+    ctx.waitUntil((async () => {
+        await setTimeoutAsync(limit * 1000);
+        while (time++ < seconds) {
+            await setTimeoutAsync(1000);
+            void (fetch("https://edge.requestcatcher.com/wait/" + time));
+            console.log(time);
+        }
+    })());
     const stream = new ReadableStream({
         start: controller => controller.enqueue(encoder.encode(String(seconds))),
         async pull(controller) {
@@ -18,13 +26,13 @@ export default async ({url}, ctx) => {
             console.log(time);
             void (fetch("https://edge.requestcatcher.com/stream/" + time));
             if (time < limit) return controller.enqueue(encoder.encode(":" + time));
-            ctx.waitUntil((async () => {
+            /*ctx.waitUntil((async () => {
                 while (time++ < seconds) {
                     await setTimeoutAsync(1000);
                     void (fetch("https://edge.requestcatcher.com/wait/" + time));
                     console.log(time);
                 }
-            })());
+            })());*/
             return controller.close();
         }
     });
