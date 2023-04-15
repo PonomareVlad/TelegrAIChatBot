5560ede2370ff999b29514e92255c9af89dec93b
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 13:10:45 +0500

    Test streaming response

--- api/test.mjs
+++ api/test.mjs
@@ -1,11 +1,20 @@
 import {promisify} from "util";
 
-const setTimeoutAsync = promisify((a, f) => setTimeout(f, a));
-
 export const config = {runtime: "edge"};
+
+const setTimeoutAsync = promisify((a, f) => setTimeout(f, a));
 
 export default async ({url}) => {
     const start = Date.now();
-    await setTimeoutAsync(new URL(url).searchParams.get("delay") || 0);
-    return new Response(JSON.stringify({time: Date.now() - start}));
+    const encoder = new TextEncoder();
+    const stream = new ReadableStream({
+        start: controller => controller.enqueue(encoder.encode("")),
+        async pull(controller) {
+            await setTimeoutAsync(new URL(url).searchParams.get("delay") || 0);
+            const json = JSON.stringify({time: Date.now() - start});
+            controller.enqueue(encoder.encode(json));
+            return controller.close();
+        },
+    });
+    return new Response(stream);
 }
