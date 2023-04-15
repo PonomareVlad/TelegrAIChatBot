702dc0ffc4c40738413ba803dc2f806091af29e2
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 20:20:53 +0500

    Test

--- api/webhook.mjs
+++ api/webhook.mjs
@@ -3,7 +3,7 @@ import configs from "../configs.json";
 
 const getURL = ({
                     headers = {},
-                    path = "api/update",
+                    path = "api/hybrid",
                     header = "x-forwarded-host",
                 }) => {
     const host = headers?.get(header) || process.env.VERCEL_URL;
