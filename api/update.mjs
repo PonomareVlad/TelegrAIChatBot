8aabfe5c1b65a0557365e9699345d36f0e140a1d
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 19:31:11 +0500

    Test

--- api/update.mjs
+++ api/update.mjs
@@ -7,24 +7,28 @@ const wait = promisify((a, f) => setTimeout(f, a));
 export default async (req, ctx) => {
     let streamController;
     const waitLimit = 85_000;
-    const requestLimit = 55_000;
+    const requestLimit = 50_000;
     const encoder = new TextEncoder();
     const stream = new ReadableStream({
         start: controller => {
             console.log("start");
+            fetch(`https://edge.requestcatcher.com/start`);
             streamController = controller;
             controller.enqueue(encoder.encode(String("OK")));
         },
         async pull(controller) {
             console.log("pull");
+            fetch(`https://edge.requestcatcher.com/pull`);
             await wait(requestLimit);
             console.log("limit");
+            fetch(`https://edge.requestcatcher.com/limit`);
             return controller.close();
         }
     });
     const handler = webhookCallback(bot, "std/http", "return", waitLimit);
     handler(req, ctx).finally(() => {
         console.log("response");
+        fetch(`https://edge.requestcatcher.com/response`);
         streamController.close();
     });
     ctx.waitUntil(wait(waitLimit));
