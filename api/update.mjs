e8f46b78acc9b3215e18c0fe36c4b1b72116fe05
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 22:27:06 +0500

    Test

--- api/update.mjs
+++ api/update.mjs
@@ -7,17 +7,9 @@ const wait = promisify((a, f) => setTimeout(f, a));
 export default async (req, ctx) => {
     let time = 0;
     let streamController;
-    const waitLimit = 90_000;
-    const requestLimit = 59_000;
-    const limit = wait(requestLimit);
+    const waitLimit = 150_000;
     const encoder = new TextEncoder();
     const handler = webhookCallback(bot, "std/http", "return", waitLimit);
-    const task = handler(req, ctx).finally(async () => {
-        await fetch(`https://edge.requestcatcher.com/response`).then(r => r.text());
-        streamController.close();
-    });
-    ctx.waitUntil(task);
-    ctx.waitUntil(wait(waitLimit));
     const stream = new ReadableStream({
         start: controller => {
             fetch(`https://edge.requestcatcher.com/start`).then(r => r.text());
@@ -26,11 +18,14 @@ export default async (req, ctx) => {
         },
         async pull(controller) {
             fetch(`https://edge.requestcatcher.com/pull`).then(r => r.text());
-            await limit;
-            ctx.waitUntil(wait(waitLimit));
+            await wait(waitLimit);
             fetch(`https://edge.requestcatcher.com/limit`).then(r => r.text());
             return controller.close();
         }
+    });
+    handler(req, ctx).finally(async () => {
+        await fetch(`https://edge.requestcatcher.com/response`).then(r => r.text());
+        streamController.close();
     });
     setInterval(() => fetch(`https://edge.requestcatcher.com/time/${time += 10}`).then(r => r.text()), 10000);
     return new Response(stream);
