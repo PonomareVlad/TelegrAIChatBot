260b35e88538fba4e14f48b6a006f9c8b6e3ef23
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-16 01:20:46 +0500

    Test

--- api/update.mjs
+++ api/update.mjs
@@ -2,38 +2,22 @@ import {promisify} from "util";
 import bot from "../src/bot.mjs";
 import {webhookCallback} from "grammy";
 
+export const config = {runtime: "edge"};
+
 const wait = promisify((a, f) => setTimeout(f, a));
 
-export default async (req, ctx) => {
-    try {
-        let time = 0;
+function edgeStream(bot, adapter = "std/http", onTimeout = "return", timeoutMilliseconds = 180_000) {
+    const callback = webhookCallback(bot, adapter, onTimeout, timeoutMilliseconds);
+    const encoder = new TextEncoder();
+    return (...args) => {
         let streamController;
-        const waitLimit = 150_000;
-        const encoder = new TextEncoder();
-        const handler = webhookCallback(bot, "std/http", "return", waitLimit);
         const stream = new ReadableStream({
-            start: controller => {
-                fetch(`https://edge.requestcatcher.com/start`).then(r => r.text());
-                streamController = controller;
-                controller.enqueue(encoder.encode(String("A few hours later ")));
-            },
-            async pull(controller) {
-                await wait(1000);
-                controller.enqueue(encoder.encode(String(".")));
-            }
+            start: controller => streamController = controller,
+            pull: controller => wait(1000).then(() => controller.enqueue(encoder.encode(".")))
         });
-        handler(req, ctx).catch(() => undefined).finally(async () => {
-            console.log("Time:", time);
-            await fetch(`https://edge.requestcatcher.com/response/${time}`).then(r => r.text());
-            streamController.close();
-        });
-        setInterval(() => fetch(`https://edge.requestcatcher.com/time/${time}`).then(r => r.text()), 10000);
-        setInterval(() => time++, 1000);
+        callback(...args).finally(() => streamController.close());
         return new Response(stream);
-    } catch (e) {
-        console.error(e);
-        return new Response(e.message);
     }
-};
+}
 
-export const config = {runtime: "edge"};
+export default edgeStream(bot);
--- api/webhook.mjs
+++ api/webhook.mjs
@@ -1,19 +1,25 @@
 import bot from "../src/bot.mjs";
 import configs from "../configs.json";
 
+const {
+    VERCEL_URL,
+    PROXY_URL = "",
+} = process.env;
+
 const getURL = ({
                     headers = {},
                     path = "api/update",
                     header = "x-forwarded-host",
                 }) => {
-    const host = headers?.get(header) || process.env.VERCEL_URL;
-    return new URL(`https://prolong-request.onrender.com/https://${host}/${path}`).href;
+    const host = headers?.get(header) || VERCEL_URL;
+    return new URL(`${PROXY_URL}https://${host}/${path}`).href;
 }
 
 export default async ({headers}) => {
-    const webhook = await bot.api.setWebhook(getURL({headers}));
+    const url = getURL({headers});
+    const webhook = await bot.api.setWebhook(url);
     const commands = await bot.api.setMyCommands(configs?.commands || []);
-    return new Response(JSON.stringify({webhook, commands}));
+    return new Response(JSON.stringify({url, webhook, commands}));
 }
 
 export const config = {runtime: "edge"};
