a7d347424cef9c19a543fb453f6f25c2f5e1fd6c
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-15 14:46:43 +0500

    Test hybrid version

--- api/hybrid.mjs
+++ api/hybrid.mjs
@@ -5,35 +5,31 @@ export const config = {runtime: "edge"};
 const setTimeoutAsync = promisify((a, f) => setTimeout(f, a));
 
 export default async ({url}, ctx) => {
-    const encoder = new TextEncoder();
+    let wait;
     let time = 0;
-    let limit = 55;
-    let seconds = parseInt(new URL(url).searchParams.get("seconds") || 100);
+    const limit = 30;
+    const seconds = parseInt(new URL(url).searchParams.get("seconds") || 100);
+    const streamPromise = new Promise(function (resolve, reject) {
+        wait = {resolve: resolve, reject: reject};
+    });
+    setInterval(() => time++, 1000);
     ctx.waitUntil((async () => {
-        await setTimeoutAsync(limit * 1000);
-        while (++time < seconds) {
+        await streamPromise;
+        while (time <= seconds) {
             await setTimeoutAsync(1000);
             void (fetch("https://edge.requestcatcher.com/wait/" + time));
             console.log(time);
         }
     })());
+    const encoder = new TextEncoder();
     const stream = new ReadableStream({
         start: controller => controller.enqueue(encoder.encode(String(seconds))),
         async pull(controller) {
-            time++;
-            if (time > seconds) return controller.close();
+            if (time >= seconds) return controller.close();
             await setTimeoutAsync(1000);
-            console.log(time);
             void (fetch("https://edge.requestcatcher.com/stream/" + time));
-            if (time < limit) return controller.enqueue(encoder.encode(":" + time));
-            /*ctx.waitUntil((async () => {
-                while (time++ < seconds) {
-                    await setTimeoutAsync(1000);
-                    void (fetch("https://edge.requestcatcher.com/wait/" + time));
-                    console.log(time);
-                }
-            })());*/
-            return controller.close();
+            controller.enqueue(encoder.encode(":" + time));
+            if (time >= limit) return controller.close(wait.resolve());
         }
     });
     return new Response(stream);
