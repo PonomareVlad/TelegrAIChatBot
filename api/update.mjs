9c2bfa0266c35d8382827e729e1d1daf89e58130
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-16 01:34:34 +0500

    Added edge-grammy package

--- api/update.mjs
+++ api/update.mjs
@@ -1,23 +1,6 @@
-import {promisify} from "util";
 import bot from "../src/bot.mjs";
-import {webhookCallback} from "grammy";
+import {webhookStream} from "edge-grammy";
 
 export const config = {runtime: "edge"};
 
-const wait = promisify((a, f) => setTimeout(f, a));
-
-function edgeStream(bot, adapter = "std/http", onTimeout = "return", timeoutMilliseconds = 180_000) {
-    const callback = webhookCallback(bot, adapter, onTimeout, timeoutMilliseconds);
-    const encoder = new TextEncoder();
-    return (...args) => {
-        let streamController;
-        const stream = new ReadableStream({
-            start: controller => streamController = controller,
-            pull: controller => wait(1000).then(() => controller.enqueue(encoder.encode(".")))
-        });
-        callback(...args).finally(() => streamController.close());
-        return new Response(stream);
-    }
-}
-
-export default edgeStream(bot);
+export default webhookStream(bot);
